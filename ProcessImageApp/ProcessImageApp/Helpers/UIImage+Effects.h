d9bee175a5184cf584c8bc93e3c734e6421617ea
Artem Kislitsyn <arcilite.kd@gmail.com>
2016-07-18 10:35:25 +0300

    add blur effect

--- ProcessImageApp/ProcessImageApp/Helpers/UIImage+Effects.h
+++ ProcessImageApp/ProcessImageApp/Helpers/UIImage+Effects.h
@@ -14,4 +14,11 @@
 - (UIImage *)negativeImage;
 - (UIImage *)horizontalMirror;
 
+- (UIImage *)applyLightEffect;
+- (UIImage *)applyExtraLightEffect;
+- (UIImage *)applyDarkEffect;
+- (UIImage *)applyTintEffectWithColor:(UIColor *)tintColor;
+
+- (UIImage *)applyBlurWithRadius:(CGFloat)blurRadius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor maskImage:(UIImage *)maskImage;
+
 @end

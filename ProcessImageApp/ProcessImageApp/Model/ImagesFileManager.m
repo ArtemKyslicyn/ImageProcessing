db00e96b97dee522eb5a1a98ff0f496c2c78eee0
Artem Kislitsyn <arcilite.kd@gmail.com>
2016-07-20 10:47:38 +0300

    exif

--- ProcessImageApp/ProcessImageApp/Model/ImagesFileManager.m
+++ ProcessImageApp/ProcessImageApp/Model/ImagesFileManager.m
@@ -59,34 +59,68 @@ NSString * const kProcessedImagesFolder = @"/ProcessedImages";
     NSString *filePath = [dataPath stringByAppendingPathComponent:filename];
   NSData * data = UIImagePNGRepresentation(image);
   [data writeToFile:filePath atomically:YES];
-  NSMutableDictionary *mutableMetadata = [[NSMutableDictionary alloc] init];
-  [mutableMetadata setObject:@"addd" forKey:(NSString *)kCGImagePropertyTIFFModel];
-  [mutableMetadata setObject:@"addd" forKey:(NSString *)kCGImagePropertyExifFocalLength];
-  [mutableMetadata setObject:@"addd" forKey:(NSString *)kCGImagePropertyExifDateTimeDigitized];
-  [mutableMetadata setObject:@"addd" forKey:(NSString *)kCGImagePropertyExifISOSpeedRatings];
-  [mutableMetadata setObject:@"addd" forKey:(NSString *)kCGImagePropertyExifDateTimeDigitized];
+  
+  CGImageSourceRef source;
+  source = CGImageSourceCreateWithData((CFDataRef)data, NULL);
+  
+  NSDictionary *metadata = (NSDictionary *) CFBridgingRelease(CGImageSourceCopyPropertiesAtIndex(source, 0, NULL));
+  
+  NSMutableDictionary *metadataAsMutable = [metadata mutableCopy];
+ 
+  
+  NSMutableDictionary *EXIFDictionary = [[metadataAsMutable objectForKey:(NSString *)kCGImagePropertyExifDictionary]mutableCopy];
+  NSMutableDictionary *GPSDictionary = [[metadataAsMutable objectForKey:(NSString *)kCGImagePropertyGPSDictionary]mutableCopy];
   
   
-  [mutableMetadata setObject:@(1.0) forKey:(__bridge NSString *)kCGImageDestinationLossyCompressionQuality];
-  
-  
-  // Create an image destination.
-  CGImageDestinationRef imageDestination = CGImageDestinationCreateWithURL((__bridge CFURLRef)[NSURL fileURLWithPath:filePath], kUTTypeJPEG , 1, NULL);
-  
-  
-  // Add your image to the destination.
-  CGImageDestinationAddImage(imageDestination, image.CGImage, (__bridge CFDictionaryRef)mutableMetadata);
-  
-  // Finalize the destination.
-  if (CGImageDestinationFinalize(imageDestination) == NO) {
-    
-    // Handle failure.
-    NSLog(@"Error -> failed to finalize the image.");
+  if(!EXIFDictionary)
+  {
+    //if the image does not have an EXIF dictionary (not all images do), then create one for us to use
+    EXIFDictionary = [NSMutableDictionary dictionary];
   }
   
-  CFRelease(imageDestination);
+  if(!GPSDictionary)
+  {
+    GPSDictionary = [NSMutableDictionary dictionary];
+  }
   
- 
+  //Setup GPS dict -
+  //I am appending my custom data just to test the logic……..
+  
+  [GPSDictionary setValue:[NSNumber numberWithFloat:1.1] forKey:(NSString*)kCGImagePropertyGPSLatitude];
+  [GPSDictionary setValue:[NSNumber numberWithFloat:2.2] forKey:(NSString*)kCGImagePropertyGPSLongitude];
+  [GPSDictionary setValue:@"lat_ref" forKey:(NSString*)kCGImagePropertyGPSLatitudeRef];
+  [GPSDictionary setValue:@"lon_ref" forKey:(NSString*)kCGImagePropertyGPSLongitudeRef];
+  [GPSDictionary setValue:[NSNumber numberWithFloat:3.3] forKey:(NSString*)kCGImagePropertyGPSAltitude];
+  [GPSDictionary setValue:[NSNumber numberWithShort:4.4] forKey:(NSString*)kCGImagePropertyGPSAltitudeRef];
+  [GPSDictionary setValue:[NSNumber numberWithFloat:5.5] forKey:(NSString*)kCGImagePropertyGPSImgDirection];
+  [GPSDictionary setValue:@"_headingRef" forKey:(NSString*)kCGImagePropertyGPSImgDirectionRef];
+  
+  [EXIFDictionary setValue:@"xml_user_comment" forKey:(NSString *)kCGImagePropertyExifUserComment];
+  //add our modified EXIF data back into the image’s metadata
+  [metadataAsMutable setObject:EXIFDictionary forKey:(NSString *)kCGImagePropertyExifDictionary];
+  [metadataAsMutable setObject:GPSDictionary forKey:(NSString *)kCGImagePropertyGPSDictionary];
+  
+  CFStringRef UTI = CGImageSourceGetType(source);
+  NSMutableData *dest_data = [NSMutableData data];
+  
+  CGImageDestinationRef destination = CGImageDestinationCreateWithData((CFMutableDataRef) dest_data, UTI, 1, NULL);
+  
+  if(!destination)
+  {
+    NSLog(@"--------- Could not create image destination---------");
+  }
+  
+  
+  CGImageDestinationAddImageFromSource(destination, source, 0, (CFDictionaryRef) metadataAsMutable);
+  
+  BOOL success = NO;
+  success = CGImageDestinationFinalize(destination);
+  
+  if(!success)
+  {
+    NSLog(@"-------- could not create data from image destination----------");
+  }
+  //[dest_data writeToFile:filePath atomically:YES];
 
   return filePath;
 }
